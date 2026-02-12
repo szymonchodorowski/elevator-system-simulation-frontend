317b42d22ef27ec7de202dca7ca9b5c0d21c3236
Szymon Chodorowski <szymon.chodorowski@gmail.com>
2026-02-12 20:11:37 +0100

    init commit

---
+++ changelog.md
@@ -0,0 +1,4 @@
+## changelog for elevator-system-simulation-frontend
+
+## 0.0.1
+- Init project
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            <h1>Elevator System Simulation</h1>
+        </div>
+    );
+}
+
+export default App;
+
